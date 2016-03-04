255250dc781dae70fdded8f319bcb3a56e5940d6
Nicolas Viennot <nicolas@viennot.biz>
2016-03-04 12:12:43 -0500

    Version Bump

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Tmate.Mixfile do
 
   def project do
     [app: :tmate,
-     version: "0.0.16",
+     version: "0.0.17",
      elixir: "~> 1.1",
      elixirc_paths: ["lib"],
      compilers: Mix.compilers,
