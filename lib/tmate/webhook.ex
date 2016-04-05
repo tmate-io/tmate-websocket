30733556cd9bb278b4007867f999f8c745951409
Nicolas Viennot <nicolas@viennot.biz>
2016-04-05 12:46:22 -0400

    Allow 2xx to be returned from webhooks, and only warn when dropping events

--- lib/tmate/webhook.ex
+++ lib/tmate/webhook.ex
@@ -35,32 +35,34 @@ defmodule Tmate.Webhook do
   def handle_cast({:emit_event, event_type, entity_id, userdata, params}, state) do
     # TODO also include timestamp
     payload = Poison.encode!(%{type: event_type, entity_id: entity_id, userdata: userdata, params: params})
-    state.urls |> Enum.each(& post_event(&1, payload, @max_retry))
+    state.urls |> Enum.each(& post_event(&1, event_type, payload, @max_retry))
     {:noreply, state}
   end
 
-  defp post_event(_url, _payload, 0=_retry_count), do: :error
-
-  defp post_event(url, payload, retry_count) do
+  defp post_event(url, event_type, payload, retry_count) do
     case post_event_once(url, payload) do
       :ok -> :ok
-      :error ->
-        :timer.sleep(@retry_interval)
-        post_event(url, payload, retry_count - 1)
+      {:error, reason} ->
+        case retry_count do
+          0 ->
+            Logger.warn "Webhook fail on #{url} - Dropping event :#{event_type} (#{reason})"
+            :error
+          _ ->
+            :timer.sleep(@retry_interval)
+            post_event(url, event_type, payload, retry_count - 1)
+        end
     end
   end
 
   defp post_event_once(url, payload) do
     headers = [{"Content-Type", "application/json"}, {"Accept", "application/json"}]
     case HTTPoison.post(url, payload, headers) do
-      {:ok, %HTTPoison.Response{status_code: 200}} ->
+      {:ok, %HTTPoison.Response{status_code: status_code}} when status_code >= 200 and status_code  < 300 ->
         :ok
       {:ok, %HTTPoison.Response{status_code: status_code}} ->
-        Logger.warn "Webhook fail on #{url}: status=#{status_code}"
-        :error
+        {:error, "status=#{status_code}"}
       {:error, %HTTPoison.Error{reason: reason}} ->
-        Logger.warn "Webhook fail on #{url}: #{reason}"
-        :error
+        {:error, reason}
     end
   end
 end
